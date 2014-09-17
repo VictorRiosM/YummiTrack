82062c51fceebabac3c1a372dc94346a07924fd5
Daniel Cardenas <danielcardeenas@gmail.com>
2014-09-16 19:43:18 -0500

    HotFix

--- app/src/main/java/com/app/yummitrack/LoginFragment.java
+++ app/src/main/java/com/app/yummitrack/LoginFragment.java
@@ -108,6 +108,14 @@ public class LoginFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
         uiHelper.onResume();
     }
 
@@ -120,14 +128,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        // For scenarios where the main activity is launched and user
-        // session is not null, the session state change notification
-        // may not be triggered. Trigger it if it's open/closed.
-        Session session = Session.getActiveSession();
-        if (session != null &&
-                (session.isOpened() || session.isClosed()) ) {
-            onSessionStateChange(session, session.getState(), null);
-        }
         uiHelper.onPause();
     }
 
